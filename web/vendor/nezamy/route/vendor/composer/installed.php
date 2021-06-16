cce1be3f4eedd006a0736df00a6bd47770a63103
naninaveen9 <32019116+naninaveen9@users.noreply.github.com>
2021-06-16 02:55:24 +0000

    y

---
+++ web/vendor/nezamy/route/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'nezamy/route',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nezamy/route' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'nezamy/support' => array(
+            'pretty_version' => 'v1.0.1',
+            'version' => '1.0.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nezamy/support',
+            'aliases' => array(),
+            'reference' => '8803292708fbfa857bdd056c9b9dc883adc51414',
+            'dev_requirement' => false,
+        ),
+    ),
+);
